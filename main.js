e176a9a28e81064ea17e0d4704b751efc0a1db71
Dai Honda <dai.honda111@gmail.com>
2021-02-15 00:41:35 +0700

    First commit

---
+++ main.js
@@ -0,0 +1,6 @@
+//var color = ["#D52A3E", "#FFFDE2", "#FF8184" , "#F12169"];
+//var i = 0
+//function changePage(){
+//    i = 1 < color.lenght ? ++1 : 0;
+//}
+document.getElementById("main").style.backgroundColor = "#D52A3E";
